9e44aae4d86c7199fc5836b0f3a0390e354dd2cd
Jerry Wang <ziyanwang1105@gmail.com>
2024-03-07 16:51:07 -0800

    add notes for function in view

--- src/view.js
+++ src/view.js
@@ -4,6 +4,12 @@ class View {
         this.meld = meld //html element from id meld
         this.setupTileSelector()
     }
+    //generate all tile
+    //add event listener for clicing the tile and it will either add to hand or meld
+    //need a tracker to track the number of tile used and cannot exceed more than 4 tiles with
+    //the same suit and number used in the hand + meld
+    //disable the tile click if adding it in will violate the above rule
+
     setupTileSelector(){
         let tileSelector = document.querySelector('#tile_selector')
         let ul = document.createElement("ul")
@@ -27,6 +33,20 @@ class View {
         }
         tileSelector.append(ul)
     }
+    //clicking the tile will append tile in hand / meld and store the tile class object in an array
+    //e is the event for clicking tile
+    //meldType is the dictionary for tracking the meld type selection
+    clickTile(e, meldType){
+
+    }
+    //clicking the meld type button will change the click tile action
+    //Meld type can be stored locally as a dictionary with default value all false, when clicking one button
+    //only the chosen one type value become true  and change all other value to false
+    //Meld type button can either be selected one or none
+    clickMeld(e){
+
+    }
+
 }
 
 export default View
